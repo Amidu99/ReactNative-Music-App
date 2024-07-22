f2c75d38bc5d0b1088c8efb70259853479156648
Amidu99 <amidushamika@gmail.com>
2024-07-22 13:26:02 +0530

    update playlist.tsx

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -1,14 +1,11 @@
 import PlaylistScreen from '@/components/screens/PlaylistScreen';
-import { AudioListProvider } from '@/contexts/AudioListContext';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Playlist() {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <AudioListProvider>
         <PlaylistScreen></PlaylistScreen>
-      </AudioListProvider>
     </SafeAreaView>
   );
 }
